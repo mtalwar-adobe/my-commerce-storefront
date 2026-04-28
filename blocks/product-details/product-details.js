44327eff408e69c493d875810487afc6934332dc
Dharmendra Kothe <dkothe@adobe.com>
2026-04-28 13:33:53 +0530

    W2 A2.1-Display and style the custom attribute using the Event

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -110,6 +110,7 @@ export default async function decorate(block) {
         </div>
         <div class="product-details__description"></div>
         <div class="product-details__attributes"></div>
+        <div class="product-details__custom-attribute"></div>
       </div>
     </div>
   `);
@@ -129,6 +130,7 @@ export default async function decorate(block) {
   const $attributes = fragment.querySelector('.product-details__attributes');
   const $tagline = fragment.querySelector('.product-details__tagline');
   const $stock = fragment.querySelector('.product-details__stock');
+  const $customAttribute = fragment.querySelector('.product-details__custom-attribute');
 
   block.replaceChildren(fragment);
   // display the custom tagline
@@ -145,6 +147,15 @@ export default async function decorate(block) {
     } else {
       $stock.textContent = '● Out of Stock';
       $stock.className = 'product-details__stock stock-badge stock-badge--out-of-stock';
+    }
+    const value = product.metaTitle;
+    if (value) {
+      $customAttribute.innerHTML = `
+      <div class="custom-attribute">
+      <dt>Custom Attribute Label</dt>
+      <dd>${value}</dd>
+      </div>
+      `;
     }
   }, { eager: true });
 
